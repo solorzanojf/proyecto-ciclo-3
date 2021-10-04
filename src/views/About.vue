82484938522beb0fc6b558644a8e9d6a40dac8b5
solorzanojf <solorzanojf@gmail.com>
2021-10-03 21:48:20 -0500

    completar informacion de la pagina

--- src/views/About.vue
+++ src/views/About.vue
@@ -8,22 +8,47 @@
         <p>
           El corregimiento de la Buitrera es un sendero de ida y vuelta de 23.3 kilómetros con tráfico moderado del municipio de Palmira Valle del Cauca, en el barrio el Carmelo está ubicada la Finca El Jordán, su agradable clima (1.289 m.s.n.m.) hace de este el lugar el sitio ideal para celebrar sus eventos especiales, hospedarse o ir de vacaciones. <br> La Finca El Jordán es un lugar generoso en armonía con la naturaleza que cuenta con un estanque con peces, y el murmullo del río en la ladera que limita la finca, desarrolla la cría de animales para el turismo sostenible, y respetuoso con la fauna. En sus instalaciones cuenta con baños con agua caliente, amplias habitaciones y wifi.  Todo esto lo puedes disfrutar con tu mejor compañía, la puesta de sol, o simplemente sentarte y contemplar el paisaje hasta el horizonte sintiéndote libre. Este hermoso lugar donde la madre naturaleza nos evoca a vivir una experiencia turística de bienestar con respeto por el medio ambiente. <br> En la Finca El Jordán contamos con capacidad de hospedar cómodamente 54 personas, siendo este el sitio perfecto para aprovechar y caminar con tranquilidad y paz, encontrando en sus alrededores tiendas de mercado, la estación de policía, restaurantes, el parque del pueblo, las iglesias, todo muy cercano, dándonos la oportunidad de conocer, permitiéndonos tener unos días más saludables y vivir con menos prisas y sin el estrés diario de la ciudad. <br> Para quienes les gusta bajar volando lo pueden hacer desde las cimas de las montañas, en un sitio especial para practicar parapente, este deporte lo puedes encontrar a pocos minutos de nuestras instalaciones, al igual que la Reserva Natural Nirvana, toda una experiencia para los visitantes. <br> Se puede llegar a La Finca El Jordán en transporte público o privado, contamos con una amplia zona de parqueadero para sus vehículos.
         </p>
-        <b-button variant="primary" :to="{name: 'About'}"><b-icon class="" icon="exclamation-circle-fill" variant="danger"></b-icon> Mas informacion</b-button>
-      </b-jumbotron>
+        </b-jumbotron>
+    <div ><b-card-group>
+    <b-card title="Mision" img-src="https://dsm01pap001files.storage.live.com/y4mRZ4GwB0QTIQAYnXqQaizFnpLMYQL1FDBfclYljls05io_zOLHc_QOXvzg39L2DFQ06T4VpLS9Qb9dSoMdST-MmWgFo2j4gDqArRZdYDcRKue5Hn2dcoPZYy8kPp5-QXUOuYJoK9vsaGkmzxuu3INdBwcgbR-Fm1sMTimcsYm5Obsp8MiWq-TqazccktQ3OfUGg9NemKMv1i3H5vVjSq7kg/mision.jpg?psid=1&width=250&height=401" img-alt="Mision" img-top>
+      <b-card-text>
+        la Finca El Jordán es una empresa que ofrece servicio de hospedaje y alquiler de espacios para la celebración de reuniones sociales, familiares, eventos empresariales e institucionales, dirigido al público que disfruta la naturaleza y la tranquilad. Estamos orientados a ofrecer un servicio de calidad con responsabilidad y atención personalizada. Contamos con personal calificado para satisfacer sus necesidades, quienes nos visitan tienen la oportunidad de disfrutar una experiencia con la naturaleza y la biodiversidad nativa de la región, sus bellos paisajes hacen de la Finca El Jordán el sitio ideal para compartir momentos inolvidables.
+      </b-card-text>
+    </b-card>
 
-      <b-card
-        title="Card Title"
-        img-src="https://picsum.photos/600/300/?image=25"
-        img-alt="Image"
-        img-top
-        tag="article"
-        class="mb-2"
-      >
-        <b-card-text>
-          Some quick example text to build on the card title and make up the bulk of the card's content.
-        </b-card-text>
+    <b-card title="Vision" img-src="https://dsm01pap001files.storage.live.com/y4mfW6IwzVFmO-oi4C1FTFkt2zLQlZLT1zS0H7ADgv5HlCSZzSWbKxz8fLHiTr_aI9D_KKdUQ2_aINTa9U_btUSQ1kUX6z0n6T6WZNxYQG1ZWu9XSUY75V0kzCZOoSPzmXnpNNdKyPlEo6AzaJrvTbauOPJC42P91S9rD8GsLQqPeoaUfZNv_AGNriKaGfK0AHo60bkPbtusXYIxnvjhHTD3g/vision.jpg?psid=1&width=250&height=400" img-alt="Vision" img-top>
+      <b-card-text>
+        en la  Finca El Jordán estamos trabajando para en el año 2030 ser reconocidos como una empresa del sector turístico con una alternativa innovadora de hospedaje, con un servicio de comidas de alta cocina, respetando la naturaleza y el medio ambiente, generando conciencia ambiental, empleo en nuestra región y turismo de bienestar. Siendo lideres en buen servicio y calidad en el Valle del Cauca.
+      </b-card-text>
+    </b-card>
 
-        <b-button href="#" variant="primary">Go somewhere</b-button>
-      </b-card>
+    <b-card title="Valores" img-src="https://dsm01pap001files.storage.live.com/y4m1BIdFWhshmGgiQU-DHRutHwotZouexR7IQZCeLixZ9VduShUFwQxM81R0mm5tIj_YEMRxrmTdM9guxmwqybx7kJWMBSEy-DRmMJbUmZnYN6bQI4JVSGGJxcg-xhXuP0eR21DuAA1GC-GqKJFx5xJeDhcGPpAstmzGIfuWSQY6iN4FRPGqvmqSnPJrJBMZtKhWdGtD4BbHxNHRW-xKQV-jg/valores.jpg?psid=1&width=250&height=399" img-alt="Valores" img-top>
+      <b-card-text>
+        En Finca El Jordán nos regimos por una serie de valores que dan identidad a nuestra empresa:<br>
+        •	Respeto<br>
+        •	Honestidad<br>
+        •	Trabajo en equipo<br>
+        •	Responsabilidad<br>
+        •	Liderazgo<br>
+        •	Calidad<br>
+        •	Buena atención
+      </b-card-text>
+    </b-card>
+  </b-card-group>
+  
+  
+</div>
+<b-jumbotron bg-variant="dark" text-variant="white" border-variant="white" class="cont mt-5 p-4" >
+        <template><h5>Sobre la Region</h5></template>
+
+        <hr class="my-3">
+
+        <p>
+          El turismo es una actividad económica importante para Colombia ya que está dentro de los países que albergan una de las mayores diversidades biológicas; nuestro país tiene la variedad más grande de aves y anfibios de todo el mundo y un gran número de plantas, reptiles y mamíferos. Colombia tiene el potencial de diferenciar su posicionamiento de turismo de naturaleza mezclando sus elementos físicos, el Valle del Cauca se distingue por la calidad humana de su gente.<br>  
+          En nuestra región contamos con una enorme variedad cultural, diversos climas y paisajes; entre hermosas montanas encontramos la Finca El Jordán un lugar apto para el turismo de naturaleza sostenible; que se caracteriza por el respeto con la fauna y la flora.<br>                  
+          El vallecaucano es de trato amable, alegre y generoso, y gracias a su capacidad de compromiso, responsabilidad y trabajo, en la Finca el Jordán hemos logrado un amplio portafolio de servicios turísticos para su distinguida clientela.
+        </p>
+        </b-jumbotron>
+      
   </div>
 </template>
