64b3d717cf5582bd206c702b857d0d2d84e9accf
solorzanojf <solorzanojf@gmail.com>
2021-10-11 20:04:19 -0500

    modificacion a modelo de usuarios

--- models/Usuarios.js
+++ models/Usuarios.js
@@ -3,11 +3,12 @@ const Schema=mongoose.Schema;
 
 
 const usuarioShema=new Schema({
+    roll: ["admin", "usuario"],
     nombres:{type:String, required:[true,'Nombre obligatorio']},
     apellidos:{type:String, required:[true,'Apellido obligatorio']},
     tipodocumento:{type:String},
-    numero_documento:{type:Number,required:[true,'Numero de Documento obligatorio']},
-    correo:{type:String,required:[true,'Correo obligatorio']},
+    numero_documento:{type:Number,required:[true,'Numero de Documento obligatorio'], unique: true},
+    correo:{type:String,required:[true,'Correo obligatorio'], unique: true},
     numero_telefonico:{type:Number,required:[true,'Numero telefono obligatorio']},
     password:{type:String,required:[true,'Password obligatorio']}
 
